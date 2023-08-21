c1375de65ae08d2ffc2773303b8d2c89050fc7ba
captain24tyagi <sarthak2002tyagi@gmail.com>
2023-08-21 12:44:51 +0530

    css-changes4

--- components/Skilll.js
+++ components/Skilll.js
@@ -14,10 +14,10 @@ export default function Skilll({ directionLeft, skill, skillprog }) {
          whileInView={{ opacity: 1, x: 0 }}
          src={skill}
          alt='/'
-         className='rounded-full object-cover md:h-20 md:w-20 h-16 w-16 border border-black'
+         className='rounded-full object-cover md:h-20 md:w-20 h-10 w-10 border border-black'
         />
 
-        <div className='absolute opacity-0 group-hover:opacity-70 transition-duration-300 ease-in-out group-hover: bg-white h-20 w-20 xl:h-24 xl:w-24 rounded-full'>
+        <div className='absolute opacity-0 group-hover:opacity-70 transition-duration-300 ease-in-out group-hover: bg-white h-10 w-10 md:h-20 md:w-20 xl:h-24 xl:w-24 rounded-full'>
           <div className='flex items-center justify-center h-full'>
             <p className='text-xl font-bold text-black opacity-100'>{skillprog}%</p>
           </div>
